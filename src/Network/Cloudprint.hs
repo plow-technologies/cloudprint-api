5aab71dc44e22c6c577c9a8d0a10fb2cb17f51a4
Kevin Cotrone <kevincotrone@gmail.com>
2014-12-29 15:48:30 -0600

    Finished uploading of a file

--- src/Network/Cloudprint.hs
+++ src/Network/Cloudprint.hs
@@ -1,55 +1,67 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Network.Cloudprint where
+module Network.Cloudprint (
+  addPrintJob
+)where
 
 import           Network.Cloudprint.Internal
 
 import           Control.Monad.IO.Class
 import           Data.ByteString
 import qualified Data.ByteString.Char8                 as B8
+import qualified Data.Either.Validation                as V
 import           Data.Monoid
+
 import           Network.Google.OAuth2
 import           Network.HTTP.Client.MultipartFormData
 import           Network.HTTP.Conduit
 import           Network.HTTP.Types                    (hAuthorization)
+import           Network.HTTP.Types.Status
 
 type PrinterId = ByteString
 type PrintContentType = ByteString
 type PrintJobName = ByteString
+type FileName = ByteString
 
 formData :: MonadIO m => PrinterId        -- ^ Cloudprint ID for the printer
                       -> PrintContentType -- ^ MIME type for the print job
                       -> PrintJobName     -- ^ Name of the print job
+                      -> FilePath
                       -> ByteString       -- ^ File contents to print
                       -> Request          -- ^ Request to add the form data to
                       -> m Request        -- ^ Resulting request
-formData printerId contentType name conts = formDataBody [
+formData printerId contentType name fn conts = formDataBody [
                     -- partBS "xsrf"                                        --  hidden text
                     partBS "printerid" printerId                                   -- hidden text
                   , partBS "jobid" ""                                       -- hidden text
                   , partBS "ticket" "{\"version\": \"1.0\",\"print\": {}}"  --  text
                   , partBS "title" name
                   , partBS "contentType" contentType
-                  , partBS "content" conts                --  file
+                  , partFileRequestBody "content" fn $ RequestBodyBS conts                --  file
                    ]
 
 
 addPrintJob :: OAuth2Client
-            -> FilePath
+            -> FilePath         -- ^ Filepath for the Oauth token 
             -> PrinterId        -- ^ Cloudprint ID for the printer
             -> PrintContentType -- ^ MIME type for the print job
             -> PrintJobName     -- ^ Name of the print job
+            -> FilePath         -- ^ Fake filename for the uploaded file
             -> ByteString       -- ^ File contents to print
-            -> IO ()
-addPrintJob client tokenFp pid ctype name conts = do
-  let addFd = formData pid ctype name conts
+            -> IO (V.Validation String ())
+addPrintJob client tokenFp pid ctype name fn conts = do
+  let addFd = formData pid ctype name fn conts 
   token <- useAccessToken client tokenFp
   case token of
     (Just t) -> do
       req <- (addFd . (authorize t)) =<< parseUrl "https://www.google.com/cloudprint/submit"
       resp <- withManager $ httpLbs req
-      return ()
-    Nothing -> do
-      return ()
+      case responseStatus resp of
+        (Status 200 _) -> return $ V.Success ()
+        _ -> return $ V.Failure (show resp)
+    Nothing -> return $ V.Failure "Failure to load token"
+    
   where authorize token request = request
                                     { requestHeaders = [(hAuthorization, B8.pack $ "Bearer " <> token)] }
+
+
 
